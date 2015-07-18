3586cd28ddd4481430ca4a983335999274724068
lvniqi <lvniqi@gmail.com>
2015-07-18 16:54:45 +0800

    添加LCR计算程序

--- python_sinc.py
+++ python_sinc.py
@@ -14,7 +14,37 @@ def change_color(in_data):
     t_r |= (in_data&0xf00000)>>10
     return hex(t_r)
 
+def LCR_Calu(freq,size,Rmin):
+    """L*C = (2*pi*freq)**2
+    Q = (W)*L/R
+    R>>0
+    L>>1**10-6
+    C>>1*10-12"""
+    Q = freq*1.0/size
+    W = 2*pi*freq
+    for L in range(1,1000):
+        L_t = L*1.0*10**-6
+        C = (1/W)**2/L_t
+        R = W*L_t/Q
+        if C <1*10**-12 or C>10**-6:
+            continue
+        elif R<Rmin:
+            continue
+        else:
+            print "L:  ",L_t
+            print "C:  ",C
+            print "R:  ",R
+            return [L_t,C,R]
+    return False
+
 if __name__ == "__main__":
-    temp = open("temp123.c",'w')
-    print>>temp,fun(4096,32)
+    #temp = open("temp123.c",'w')
+    #print>>temp,fun(4096,32)
     
+    for x in range(0,30):
+        if LCR_Calu(21000,4000,x):
+            continue
+        else:
+            print x-1
+            break
+
